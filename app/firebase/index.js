b389e42cbeda2305f53ce164ca767ebb326bda6f
Udeep <udeep@nagarro.com>
2018-02-12 16:53:46 +0530

    config for firebase

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -1,13 +1,12 @@
 import firebase from 'firebase'
 
-
   var config = {
-    apiKey: "AIzaSyDuOvT-HvzuHXgze0lTdiiAHwbh5W2-Ut0",
-    authDomain: "todo-app-252f1.firebaseapp.com",
-    databaseURL: "https://todo-app-252f1.firebaseio.com",
-    projectId: "todo-app-252f1",
-    storageBucket: "todo-app-252f1.appspot.com",
-    messagingSenderId: "1030340219916"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
   firebase.initializeApp(config);
 export var firebaseRef = firebase.database().ref();
